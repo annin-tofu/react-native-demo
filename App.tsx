dd114102367f10740e37fd66bb2fb3adb3a840af
annin-tofu <yuok5770@gmail.com>
2021-07-22 01:10:15 +0900

    1st

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const name = "Yuya"; //Weakly Typed Language (TypeScript)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
+
+      {/* Product */}
+      {/* Product */}
+      {/* Product */}
+      {/* Product */}
+      {/* Product */}
+      {/* Product */}
     </View>
   );
 }
@@ -14,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # react-native-demo
+
+## React-Native uses Column (flex) by default
+
+unlike other platform
+
+# TypeScript is less strict than JS
+
+const name = "Yuya"; //Weakly Typed Language (TypeScript)
+
+    const name: string = "Yuya"; //Weakly Typed Language (TypeScript) ->Strongly Typed (JS)
+
+    0:53
+
+### ? For “optional” in props
+
+#
